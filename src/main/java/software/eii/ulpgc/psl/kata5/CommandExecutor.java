88cd8b86bc2a486fe23534d5a39dc0f9c297f475
PabloSeguraLopez <pabseglop@gmail.com>
2023-12-28 20:13:58 +0000

    CommandExecutor implemented

---
+++ src/main/java/software/eii/ulpgc/psl/kata5/CommandExecutor.java
@@ -0,0 +1,30 @@
+package software.eii.ulpgc.psl.kata5;
+
+import spark.Request;
+import spark.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandExecutor {
+    private static Map<String, Command> commands = new HashMap<>();
+    private Request request;
+    private Response response;
+
+    private CommandExecutor(Request request, Response output) {
+        this.request = request;
+        this.response = output;
+    }
+    public static CommandExecutor with(Request req, Response rep){
+        return new CommandExecutor(req, rep)
+    }
+    public static void put(String name, Command command){
+        commands.put(name, command);
+    }
+    public String execute(String name){
+        Command.Output output = commands.get(name).execute(q->request.queryParams(q));
+        response.status(output.response());
+        return output.result();
+    }
+
+}
